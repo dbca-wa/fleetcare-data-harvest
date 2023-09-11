2090d2509f70d4c825ad84f19daa2f0cc5b1c3fd
Ashley Felton <ashley@ropable.com>
2023-09-11 11:52:09 +0800

    Project init.

---
+++ README.md
@@ -0,0 +1,45 @@
+# Azure Function: Fleetcare tracking data harvest
+
+This is a basic Azure Function to harvest Fleetcare tracking data into a
+database, triggered by a BlobTrigger event.
+
+# Documentation references
+
+Azure Functions developer guide:
+https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference?tabs=blob
+
+Azure Functions Python developer guide:
+https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference-python?tabs=asgi%2Capplication-level&pivots=python-mode-decorators
+
+Develop Azure Functions locally using Core Tools:
+https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=linux%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-python
+
+Create a Python Python from the command line:
+https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-cli-python?tabs=linux%2Cbash%2Cazure-cli&pivots=python-mode-configuration
+
+Azure blob storage trigger for Azure Functions:
+https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-blob-trigger?tabs=python-v2%2Cin-process&pivots=programming-language-python
+
+
+# Local development
+
+Install the Azure Functions Core Tools, as per the links above.
+
+Create and activate a local Python virtualenv (currently tested in Python 3.10),
+then install the packages in `requirements.txt`. Create a `local.settings.json`
+file to configure function, containing the following values:
+
+```json
+{
+  "IsEncrypted": false,
+  "Values": {
+    "FUNCTIONS_WORKER_RUNTIME": "python",
+    "AzureWebJobsFeatureFlags": "EnableWorkerIndexing",
+    "AzureWebJobsStorage": "<CONNECTION STRING FROM STORAGE ACCOUNT>",
+    "DATABASE_URL": "<DATABASE CONNECTION STRING>",
+    "STORAGE_CONNECTION_STRING": "<CONNECTION STRING FROM STORAGE ACCOUNT>"
+  }
+}
+```
+
+Make changes and run the function locally using `func start`.
---
+++ function_app.py
@@ -0,0 +1,76 @@
+import azure.functions as func
+from datetime import datetime, timedelta
+import json
+import os
+from pytz import timezone
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+
+app = func.FunctionApp()
+awst = timezone("Australia/Perth")
+# Define DATABASE_URL in environment vars in the format postgresql+psycopg://user:password@hostname/database
+database_url = os.environ.get("DATABASE_URL", None)
+
+
+@app.function_name(name="FleetcareTrackingData")
+@app.blob_trigger(arg_name="blob", path="tracking", connection="STORAGE_CONNECTION_STRING")
+def main(blob: func.InputStream):
+    data = json.loads(blob.read())
+    # Reference: https://learn.microsoft.com/en-us/python/api/azure-functions/azure.functions.blob.inputstream
+
+    # Only process blobs having a timestamp newer than BLOB_PROCESS_THRESHOLD (minutes).
+    # This is to reduce the incidence of the function trying to process all blobs uploaded since
+    # the dawn of time.
+    timestamp = datetime.strptime(data["timestamp"], "%d/%m/%Y %I:%M:%S %p").astimezone(awst)
+    threshold = int(os.environ.get("BLOB_PROCESS_THRESHOLD", 1440))  # Default to a threshold of one day.
+    since = datetime.now().astimezone(awst) - timedelta(minutes=threshold)
+    if timestamp < since:
+        return
+
+    engine = create_engine(database_url)
+
+    with engine.connect() as conn:
+        conn.execute(text("SET TIMEZONE = 'Australia/Perth'"))  # Ensure that TZ is set correctly.
+        source_device_type = "fleetcare"
+        deviceid = data["vehicleID"]
+        device_sql = text(f"SELECT id FROM tracking_device WHERE source_device_type = '{source_device_type}' AND deviceid LIKE '%{deviceid}'")
+        device = conn.execute(device_sql).fetchone()
+
+        if device:  # Only insert a loggedpoint if we matched a deviceid.
+            coords = data["GPS"]["coordinates"]
+            point = f"SRID=4326;POINT({coords[0]} {coords[1]})"
+            heading = float(data["readings"]["vehicleHeading"]) if data["readings"]["vehicleHeading"] else 0
+            velocity = float(data["readings"]["vehicleSpeed"]) if data["readings"]["vehicleSpeed"] else 0
+            altitude = float(data["readings"]["vehicleAltitude"]) if data["readings"]["vehicleAltitude"] else 0
+            seen = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            device_id = device[0]
+            message = 3
+            loggedpoint_sql = text(
+                f"""INSERT INTO tracking_loggedpoint (
+                    point,
+                    heading,
+                    velocity,
+                    altitude,
+                    seen,
+                    device_id,
+                    message,
+                    source_device_type,
+                    raw
+                )
+                VALUES (
+                    '{point}'::geometry,
+                    {heading},
+                    {velocity},
+                    {altitude},
+                    '{seen}'::timestamp with time zone,
+                    {device_id},
+                    {message},
+                    '{source_device_type}',
+                    '{blob.name}'
+                )"""
+            )
+            conn.execute(loggedpoint_sql)
+            conn.commit()
+
+        conn.close()
