f31b2410901da05cf013b6a72ac3fd12b7a0832b
Ashley Felton <ashley@ropable.com>
2026-03-25 09:12:23 +0800

    Fix unit tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,21 +14,21 @@ with open(os.path.join(os.path.dirname(__file__), "data.json"), "rb") as f:
 
 
 def test_blob_created_event():
-    assert handle_blob_created_event(_data)
+    assert handle_blob_created_event(_data, blob_url="http://fake.blob.url")
 
 
 def test_blob_created_event_new_device():
     _data["vehicleID"] = str(random.randint(1000000, 9999999))
-    assert handle_blob_created_event(_data)
+    assert handle_blob_created_event(_data, blob_url="http://fake.blob.url")
 
 
 def test_blob_created_event_new_rego():
     then = datetime.now() - timedelta(minutes=2)
     _data["timestamp"] = then.strftime("%d/%m/%Y %I:%M:%S %p")
-    assert handle_blob_created_event(_data)
+    assert handle_blob_created_event(_data, blob_url="http://fake.blob.url")
 
     _data["vehicleRego"] = f"1{''.join(random.choices(string.ascii_uppercase, k=6))}"
     then = datetime.now() - timedelta(minutes=1)
     _data["timestamp"] = then.strftime("%d/%m/%Y %I:%M:%S %p")
 
-    assert handle_blob_created_event(_data)
+    assert handle_blob_created_event(_data, blob_url="http://fake.blob.url")
